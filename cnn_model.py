cfb6604b4e55910c274c5fff29dde6efcdf59205
furkankinli <furkan.kinli@ozu.edu.tr>
2017-05-25 01:13:10 +0300

    Update cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -11,7 +11,8 @@ def CNN(inputs, feature_maps, conv_stride, maxp_stride, is_training=True, x_size
     batch_norm_params = {'is_training': is_training, 'decay': 0.9, 'updates_collections': None}
     with slim.arg_scope([slim.conv2d, slim.fully_connected],
                         normalizer_fn=slim.batch_norm,
-                        normalizer_params=batch_norm_params):
+                        normalizer_params=batch_norm_params,
+                        weights_regularizer=slim.l2_regularizer(0.001)):
         x = tf.reshape(inputs, [-1, x_size, x_size, 3])
 
         # For slim API, by default the activation function is rectified linear unit "activation_fn=nn.relu"
@@ -21,12 +22,13 @@ def CNN(inputs, feature_maps, conv_stride, maxp_stride, is_training=True, x_size
         net = slim.conv2d(net, 2 * feature_maps, conv_stride, scope='conv2-1')
         net = slim.max_pool2d(net, maxp_stride, scope='pool2')
         net = slim.conv2d(net, 2 * 2 * feature_maps, conv_stride, scope='conv3-1')
+        net = slim.conv2d(net, 2 * 2 * feature_maps, conv_stride, scope='conv4-1')
         net = slim.max_pool2d(net, maxp_stride, scope='pool3')
         net = slim.flatten(net, scope='flatten1')
-        net = slim.fully_connected(net, 4096, scope='fc1')
+        net = slim.fully_connected(net, 2048, scope='fc1')
         net = slim.dropout(net, is_training=is_training, scope='dropout1')
         net = slim.fully_connected(net, 1024, scope='fc2')
-        net = slim.dropout(net, is_training=is_training, scope='dropout1')  # 0.5 by default
-        outputs = slim.fully_connected(net, 8, activation_fn=None, normalizer_fn=None, scope='fco')
+        net = slim.dropout(net, is_training=is_training, scope='dropout2')  # 0.5 by default
+        outputs = slim.fully_connected(net, 5, activation_fn=None, normalizer_fn=None, scope='fco')
 
     return outputs
